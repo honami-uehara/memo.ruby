dd3de53892e7512e3271ce1e6d84c71acb36b79c
honami-uehara <honaa_cos.627i@icloud.com>
2021-12-07 14:02:16 +0000

    提出

---
+++ hello.rb
@@ -0,0 +1,41 @@
+
+require "csv"
+
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+
+memo_type = gets.to_i
+
+if memo_type == 1
+
+ puts "拡張子を除いたファイル名を入力してください"
+
+ memo_name = gets.chomp
+
+ puts "メモしたい内容を記述してください。"
+ puts "完了したらCtrl+Dを押します。"
+
+ input = readlines
+
+ CSV.open("#{memo_name}.csv","w") do |csv|
+  csv << input
+　 end
+
+ elsif memo_type == 2
+
+ puts "拡張子を除いたファイル名を入力してください。"
+
+ memo_name = gets.chomp
+
+ puts "メモしたい内容を記述してください。"
+ puts "完了したらCtrl+Dを押します。"
+
+ input = readlines
+
+ CSV.open("#{memo_name}.csv","a") do |csv|
+  csv << input
+   end
+
+else
+  puts "1 or 2　選択し直して下さい"
+end
+
